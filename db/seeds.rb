5d06cf590558e0984e8b7ca878bdb41d65e7aa93
Adam Mazur <adammazue@gmail.com>
2021-03-03 14:51:46 -0700

    Created Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+catagories = ["title", "author"]
+10.times do |i|
+  Book.create(
+    title: "#{Faker::DcComics.hero}, #{Faker::DcComics.heroine}",
+    author: Faker::Artist.name
+  )
+end 
